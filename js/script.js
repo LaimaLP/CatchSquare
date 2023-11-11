7b0918cfad82a6ab267589fe6e7c13a5287a24ca
Laima <pileliene.laima@gmail.com>
2023-11-12 01:20:06 +0200

    player and opponent count DOM

--- js/script.js
+++ js/script.js
@@ -7,39 +7,52 @@ document.addEventListener("DOMContentLoaded", function () {
     squareDOM.style.backgroundColor = `#${randomColor}`;
   };
   const changePosition = () => {
+    if(isHit===false){
+      opponentCount++
+    }
+    isHit=false;
+    opponentCountDOM.innerText = opponentCount;
+
+    console.log("opp:",opponentCount)
     const randomTop = Math.floor(Math.random() * 400);
     const randomLeft = Math.floor(Math.random() * 750);
     squareDOM.style.top = `${randomTop}px`;
     squareDOM.style.left = `${randomLeft}px`;
+    document.getElementById("square").style.display= "block"
+generateHexColor();
   };
 
-
   changePosition();
-  generateHexColor();
-  shoot();
-  setInterval(generateHexColor, 1000);
-  setInterval(changePosition, 1000);
+  setInterval(changePosition, 2000);
 
 });
 
 
+const nameDOM = document.getElementById("playerName");
 
-document.getElementById("square").addEventListener("click", shoot);
-function shoot(){
-    document.getElementById("square").style.borderRadius= "50px";
 
-    setTimeout(() => {
-        document.getElementById("square").style.borderRadius= "0";
-      }, 400);
+let playerCountDOM = document.getElementById("playerCount")
+let opponentCountDOM = document.getElementById("opponentCount")
+let playerCount = 0;
+let opponentCount = 0;
+let isHit = false;
+document.getElementById("square").addEventListener("click", hit);
+
+function hit(){
+    playerCount++;
+    playerCountDOM.innerText = playerCount;
+    console.log("my",playerCount)
+    document.getElementById("square").style.display= "none"
+    isHit=true;
+    
 }
 
 
+console.log(playerCount)
 
   let time = 30;
-
   const countDownDOM = document.getElementById('countDown');
 
-  
   function countDown() {
     
     countDownDOM.innerText = time;
@@ -47,13 +60,14 @@ function shoot(){
     
     if (time === 0) {
       clearInterval(timerInterval);
-      alert("Uuups, time out! ");
+      alert("Oops, time out! ");
     } else {
       time--;
     }
   }
+  // const timerInterval = setInterval(countDown, 1000);
 
-  const timerInterval = setInterval(countDown, 1000);
+
 
 
 
