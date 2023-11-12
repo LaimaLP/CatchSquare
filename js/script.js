5ae1d80c72ecdeeb1a5bcbf727b02cfc06f69de2
Laima <pileliene.laima@gmail.com>
2023-11-12 15:14:53 +0200

    form prevent defoult

--- js/script.js
+++ js/script.js
@@ -1,84 +1,86 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const squareDOM = document.getElementById("square");
 
-  const generateHexColor = () => {
-    const randomColor = Math.random().toString(16).slice(2, 8);
+const formDOM = document.querySelector("form");
+const nameBtnDOM = document.querySelector(".nameBtn");
+const squareDOM = document.getElementById("square");
+const startBtnDOM = document.querySelector(".startBtn");
+let playerCountDOM = document.getElementById("playerCount");
+let opponentCountDOM = document.getElementById("opponentCount");
+const countDownDOM = document.getElementById("countDown");
+const playerGameInfoDOM = document.querySelectorAll(".playerGameInfo")
 
-    squareDOM.style.backgroundColor = `#${randomColor}`;
-  };
-  const changePosition = () => {
-    if(isHit===false){
-      opponentCount++
-    }
-    isHit=false;
-    opponentCountDOM.innerText = opponentCount;
-
-    console.log("opp:",opponentCount)
-    const randomTop = Math.floor(Math.random() * 400);
-    const randomLeft = Math.floor(Math.random() * 750);
-    squareDOM.style.top = `${randomTop}px`;
-    squareDOM.style.left = `${randomLeft}px`;
-    document.getElementById("square").style.display= "block"
-generateHexColor();
-  };
-
-  changePosition();
-  // setInterval(changePosition, 2000);
-
-});
-
-function playerNameInput() {
-  let nameDOM = document.getElementById("playerName");
-  let inputField = document.getElementById("nameInput");
-
-  document.querySelector(".nameBtn").addEventListener("click", () => {
-      nameDOM.innerText = inputField.value;
-  });
-}
-
-// Call the function to set up the event listener
-playerNameInput();
-
-let playerCountDOM = document.getElementById("playerCount")
-let opponentCountDOM = document.getElementById("opponentCount")
 let playerCount = 0;
 let opponentCount = 0;
 let isHit = false;
-document.getElementById("square").addEventListener("click", hit);
-
-function hit(){
-    playerCount++;
-    playerCountDOM.innerText = playerCount;
-    console.log("my",playerCount)
-    document.getElementById("square").style.display= "none"
-    isHit=true;
-    
-}
+let time = 30;
 
 
-console.log(playerCount)
 
-  let time = 30;
-  const countDownDOM = document.getElementById('countDown');
 
+nameBtnDOM.addEventListener("click", function (event) {
+  event.preventDefault();
+const inputField = document.getElementById("nameInput");
+const nameDOM = document.getElementById("playerName");
+
+
+  nameDOM.textContent = inputField.value[0].toUpperCase() + inputField.value.slice(1);
+  formDOM.classList.add("notShow");
+
+  playerGameInfoDOM.forEach(element => {
+    element.classList.add("showPlayers");
+  });
+});
+
+
+
+startBtnDOM.addEventListener("click", function () {
   function countDown() {
-    
     countDownDOM.innerText = time;
-
-    
+  
     if (time === 0) {
       clearInterval(timerInterval);
-      alert("Oops, time out! ");
+      alert(`Oops, time out! Your scrore: ${playerCount}:${opponentCount}`);
     } else {
       time--;
     }
   }
-  // const timerInterval = setInterval(countDown, 1000);
+  const timerInterval = setInterval(countDown, 1000);
+
+  const generateHexColor = () => {
+    const randomColor = Math.random().toString(16).slice(2, 8);
+    squareDOM.style.backgroundColor = `#${randomColor}`;
+  };
+
+  const changePosition = () => {
+    if (isHit === false) {
+      opponentCount++;
+    }
+    isHit = false;
+    opponentCountDOM.innerText = opponentCount;
+    
+    const randomTop = Math.floor(Math.random() * 400);
+    const randomLeft = Math.floor(Math.random() * 750);
+    squareDOM.style.top = `${randomTop}px`;
+    squareDOM.style.left = `${randomLeft}px`;
+    squareDOM.style.display = "block";
+    generateHexColor();
+  };
+// changePosition()
+   setInterval(changePosition, 1000);
+});
 
 
 
+squareDOM.addEventListener("click", hit);
+
+function hit() {
+  playerCount++;
+  playerCountDOM.innerText = playerCount;
+  // console.log("my", playerCount);
+  squareDOM.style.display = "none";
+  isHit = true;
+}
+
+console.log(playerCount);
 
 
 
-
-  
